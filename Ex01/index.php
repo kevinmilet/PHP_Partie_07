3269abb59f3056b88a77ec6911e31a15d742d55a
kevinmilet <kevin.milet@gmail.com>
2020-12-10 11:14:49 +0100

    partie 7 ex 1 ok

---
+++ Ex01/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Partie 7 - Exercice 1</title>
+</head>
+<body>
+
+    <form action="user.php" methode="get">
+        <label for="lastname">Nom: </label>
+        <input type="text" id="lastname" name="lastname">
+        <label for="firstname">Prénom: </label>
+        <input type="text" id="firstname" name="firstname">
+        <button type="submit">Envoyer</button>
+    </form>
+    
+</body>
+</html>
---
+++ Ex01/user.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Partie 7 - Exercice 1</title>
+</head>
+<body>
+    <p>Bonjour <?php echo $_GET['firstname'] . ' ' . $_GET['lastname']?></p>
+    
+</body>
+</html>
