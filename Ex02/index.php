2f29b58bea6a5d4c043e714b054f8296b844170f
kevinmilet <kevin.milet@gmail.com>
2020-12-10 11:29:48 +0100

    partie 7 ex 2 ok

---
+++ Ex02/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Partie 7 - Exercice 2</title>
+</head>
+<body>
+
+    <form action="user.php" method="post">
+        <label for="lastname">Nom: </label>
+        <input type="text" id="lastname" name="lastname">
+        <label for="firstname">Prénom: </label>
+        <input type="text" id="firstname" name="firstname">
+        <button type="submit">Envoyer</button>
+    </form>
+    
+</body>
+</html>
